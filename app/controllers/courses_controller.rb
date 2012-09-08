aa06b1b16f4732811230329f7fea749fc336e625
John Yan <yan.jianhua@gmail.com>
2012-09-08 13:01:28 +0800

    update 20908 home

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,7 +4,6 @@ class CoursesController < ApplicationController
   def index
     if current_user.teacher?
       @course  = Course.new
-      @courses = current_user.courses
       render "t_dashboard"
     else
       
@@ -13,11 +12,19 @@ class CoursesController < ApplicationController
   end
   
   def show
-    @course = params[:id]
+    @course = Course.find(params[:id])
     
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @sclass }
+    end
+  end
+  
+  def edit
+    @course = Course.find(params[:id])
+    
+    respond_to do |format|
+      format.html # edit.html.erb
     end
   end
   
@@ -35,6 +42,45 @@ class CoursesController < ApplicationController
       end
     end
   end
+  
+  def update
+    @course = Course.find(params[:id])
+
+    respond_to do |format|
+      if @course.update_attributes(params[:course])
+         @course.update_user(current_user,0)
+        format.html { redirect_to root_url, notice: t('operate.update_success') }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @course.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def destroy
+    @course = Course.find(params[:id])
+    @course.delete_users
+    @course.destroy
+
+    respond_to do |format|
+      format.html { redirect_to root_url }
+      format.json { head :ok }
+    end
+  end 
+  
+  def operate
+    @course = Course.find(params[:id])
+    case params[:by]
+    when "add_teacher"
+
+    when "remove_teacher"
+      
+    else
+    end
+    
+  end
+  
   
   
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -7,6 +7,14 @@ class Course < ActiveRecord::Base
   end
   
   def add_user(user,link_type)
-    Courseuser.create(:course_id=>self.id,:coursename=>self.title,:link_id=>user.id,:linkname=>user.name,:ltype=>link_type)
+    Courseuser.create(:course_id=>self.id,:coursename=>self.title,:link_id=>user.id,:linkname=>user.name,:ltype=>link_type,onumber=>self.ccode)
+  end
+  
+  def update_user(user,link_type)
+    Courseuser.where(:course_id =>self.id,:link_id => user.id, :ltype=>link_type).first.update_attributes(:coursename=>self.title,:linkname=>user.name,:onumber=>self.ccode)
+  end
+  
+  def delete_users
+    Courseuser.delete_all(:course_id=>self.id)
   end
 end
